d53ca5522e06cfd3c56c051d33fc080335743544
Iceing <1948049031@qq.com>
2015-11-17 11:37:47 +0800

    Initial Commit

---
+++ iOS8/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS8
+//
+//  Created by 蔡跃明 on 15/11/17.
+//  Copyright © 2015年 lm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS8/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS8
+//
+//  Created by 蔡跃明 on 15/11/17.
+//  Copyright © 2015年 lm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
